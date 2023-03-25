f171aa1ac83772ec6e0c00e4d3dd1802c92fd7f8
Fabry99 <irving.abrego@catolica.edu.sv>
2023-03-25 10:04:38 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base (options) { 
+        
+            
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+
+        public DbSet<estados_equipo>estados_equipo { get; set; }
+
+        public DbSet<estados_reservas>estados_reserva {get; set; }
+
+        public DbSet<facultades> facultades { get; set;}
+
+        public DbSet<tipo_equipo> tipo_equipo { get;set; }
+
+
+    }
+}
